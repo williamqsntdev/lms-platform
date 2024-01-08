7421e09345d9f5fe92952ee2215fffc70d514514
williamqsntdev <quartz_09kudzus@icloud.com>
2024-01-08 20:21:10 +0100

    first commit

---
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -0,0 +1,8 @@
+
+const SearchPage = () => {
+    return ( 
+        <div>This is a serach page!</div>
+     );
+}
+ 
+export default SearchPage;
---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Logo() {
+    return(
+        <Image height={130} width={130} alt="logo" src="/logo.svg" />
+    )
+}
