5b02ee5c899e38d8718dc9a107734d47446b4335
williamqsntdev <quartz_09kudzus@icloud.com>
2024-01-08 22:46:39 +0100

    add title and description form

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { ToastProvider } from '@/components/toaster-provider'
+import { ToastProvider } from '@/components/providers/toaster-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
